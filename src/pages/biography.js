5e7b59f274ff1bf39f14b4968ec092b3559b5e97
Hand Whittled <company@handwhittled.com>
2017-09-26 17:49:27 -0400

    First stab at Biography page

--- src/pages/biography.js
+++ src/pages/biography.js
@@ -1,10 +1,58 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import marked from 'marked'
 import styles from './biography.module.scss'
 
-export default ({ data }) =>
-  <section>
-    <header className={styles.header}>
-      <h1 className={styles.title}>Recordings</h1>
-    </header>
-  </section>
+export default class Biography extends React.Component {
+  render() {
+    const node = this.props.data.allContentfulBiography.edges[0].node
+    const paragraphs = node.biography.biography.split('\n').filter(paragraph => paragraph !== '')
+    const renderedBiography = paragraphs.map((paragraph, index) =>
+      <div className={styles.paragraph} dangerouslySetInnerHTML={{ __html: marked(paragraph) }} key={index} />)
+
+    return (
+      <section>
+        <header className={styles.header}>
+          <h1 className={styles.title}>Biography</h1>
+        </header>
+
+        <div className={styles.wrapper}>
+          {renderedBiography}
+        </div>
+      </section>
+    )
+  }
+}
+
+export const query = graphql`
+  query Biography {
+    allContentfulBiography {
+      edges {
+        node {
+          id
+          color
+          biography {
+            id
+            biography
+          }
+          longBiography {
+            id
+            file {
+              url
+              fileName
+              contentType
+            }
+          }
+          shortBiography {
+            id
+            file {
+              url
+              fileName
+              contentType
+            }
+          }
+        }
+      }
+    }
+  }
+`
