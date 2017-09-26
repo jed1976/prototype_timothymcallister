c3b7380e4d138eb9afecbf34f0dbbbe306c892c9
Hand Whittled <company@handwhittled.com>
2017-09-25 21:37:54 -0400

    Changed the look of the audio player.

--- src/components/recording.js
+++ src/components/recording.js
@@ -70,21 +70,26 @@ class Recording extends React.Component {
           <div className={styles.imageWrapper}>
             <img
               className={styles.image}
-              onClick={this.toggleMedia}
-              onTouchStart={() => { }}
               src={this.props.imageSrc}
               srcSet={this.props.imageSrcSet}
-              title="Play sample"
             />
           </div>
 
           <div className={styles.textWrapper}>
-            <div className={styles.progressTrack}>
+            <div className={styles.progressTrack} onClick={this.toggleMedia} onTouchStart={() => { }} title="Play sample">
               <span
                 className={styles.progress}
                 ref={progress => this.progress = progress}
                 style={{ backgroundColor: this.props.color, width: this.state.progress }}
-              ></span>
+              >
+                {this.props.media !== '' ? (
+                  <span className={styles.headphones}>
+                    <svg style={{ fill: 'currentColor' }} viewBox="0 0 24 18">
+                      <path d="M12,0 C7.88625024,0 4.43625024,2.895 3.57,6.75 C3.65812512,6.74062512 3.75,6.72 3.84,6.72 C4.08562512,6.72 4.32187488,6.77062512 4.545,6.84 C5.35875024,3.47437488 8.38687488,0.96 12,0.96 C15.6131251,0.96 18.6412502,3.47437488 19.455,6.84 C19.6781251,6.77062512 19.9143749,6.72 20.16,6.72 C20.25,6.72 20.3418749,6.74062512 20.43,6.75 C19.5637502,2.895 16.1118749,0 12,0 Z M3.84,7.68 C3.04687488,7.68 2.4,8.32687488 2.4,9.12 L2.4,16.32 C2.4,17.1131251 3.04687488,17.76 3.84,17.76 C4.63312512,17.76 5.28,17.1131251 5.28,16.32 L5.28,9.12 C5.28,8.32687488 4.63312512,7.68 3.84,7.68 Z M20.16,7.68 C19.3668749,7.68 18.72,8.32687488 18.72,9.12 L18.72,16.32 C18.72,17.1131251 19.3668749,17.76 20.16,17.76 C20.9531251,17.76 21.6,17.1131251 21.6,16.32 L21.6,9.12 C21.6,8.32687488 20.9531251,7.68 20.16,7.68 Z M1.44,9.18 C0.57375024,9.87562512 0,11.1806251 0,12.72 C0,14.2593749 0.57375024,15.5643749 1.44,16.26 L1.44,9.18 Z M22.56,9.18 L22.56,16.26 C23.4262502,15.5643749 24,14.2593749 24,12.72 C24,11.1806251 23.4262502,9.87562512 22.56,9.18 Z" />
+                    </svg>
+                  </span>
+                ) : ''}
+              </span>
             </div>
 
             <h1 className={styles.title} style={{ color: this.props.color }}>{this.props.title}</h1>
