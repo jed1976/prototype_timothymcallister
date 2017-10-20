8cd51e9bb1b9cf46d5885d70962e0644ab3c68f3
Hand Whittled <company@handwhittled.com>
2017-10-20 09:39:54 -0400

    Testing favicon directory placement.

--- src/html.js
+++ src/html.js
@@ -26,10 +26,7 @@ module.exports = class HTML extends React.Component {
           {/* Meta */}
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="apple-mobile-web-app-title" content="Timothy McAllister" />
           <meta name="application-name" content="Timothy McAllister" />
 
